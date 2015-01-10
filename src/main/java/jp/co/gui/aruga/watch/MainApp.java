9af899ce9a766f661a822d49d7982280b763b336
akari <akari.akeboshi@gmail.com>
2015-01-10 15:20:18 +0900

    * あ

--- src/main/java/jp/co/gui/aruga/watch/MainApp.java
+++ src/main/java/jp/co/gui/aruga/watch/MainApp.java
@@ -5,19 +5,40 @@ import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class MainApp extends Application {
-
+Label label;
+    TextField field;
+    Button button;
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+       /* Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+                
         
         Scene scene = new Scene(root);
         scene.getStylesheets().add("/styles/Styles.css");
         
         stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();*/
+        label = new Label("This is JavaFX!");
+        field = new TextField("test");
+        button = new Button("Click");
+        button.setOnAction((ActionEvent)-> {
+            String msg = "you typed: " + field.getText();
+            label.setText(msg);
+        });
+        BorderPane pane = new BorderPane();
+        pane.setTop(label);
+        pane.setCenter(field);
+        pane.setBottom(button);
+        Scene scene = new Scene(pane, 320, 120);
         stage.setScene(scene);
         stage.show();
     }
